2d16d1d45dcfcbcd9e6273cde3e0578db28f45a8
Doormat1 <68517771+Doormat1@users.noreply.github.com>
2025-12-17 19:54:07 +0000

    Updated 2nd state to new animation melting

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ const ENERGY_NEEDED_TO_BOIL = 250; // arbitrary units
 
 const states = [
     { temp: -19.9, image: 'https://doormat1.github.io/solid_heating.gif', status: 'Frozen ice cube' },
-    { temp: 0, image: 'https://doormat1.github.io/Ice-Cube-2.gif', status: 'Ice melting (0°C)' },
+    { temp: 0, image: 'https://doormat1.github.io/Melting.gif.gif', status: 'Ice melting (0°C)' },
     { temp: 0.1, image: 'https://doormat1.github.io/Water-heating.gif', status: 'Liquid water heating up' },
     { temp: 100, image: 'https://doormat1.github.io/boiling-water.gif', status: 'Water boiling (100°C)' },
     { temp: 100.1, image: 'https://doormat1.github.io/Steam.gif', status: 'Water has evaporated, now heating the steam' }
